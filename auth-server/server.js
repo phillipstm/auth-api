e9ed074fd832a9fad5e17a27b7b3ea7370152df2
Teresa <luvmargaritas@gmail.com>
2022-09-29 07:40:15 -0500

    added more content

--- auth-server/server.js
+++ auth-server/server.js
@@ -8,7 +8,7 @@ const morgan = require('morgan');
 // Esoteric Resources
 const errorHandler = require('./error-handlers/500.js');
 const notFound = require('./error-handlers/404.js');
-const authRoutes = require('./auth/router/index.js');
+const authRoutes = require('./auth/routes.js');
 
 // Prepare the express app
 const app = express();
@@ -29,7 +29,7 @@ app.use(errorHandler);
 
 module.exports = {
   server: app,
-  startup: (port) => {
+  start: (port) => {
     app.listen(port, () => {
       console.log(`Server Up on ${port}`);
     });
